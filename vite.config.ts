58b79bee384edfb3e43b2c4257b158b3e93ea520
Dustin Weber <dustinpaulweber@gmail.com>
2025-08-21 12:56:05 -0400

    vitechange

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Optional: Custom plugin to remove version specifiers (if you really need it)
-const importRegex = /(['"])(.+?)@\d+\.[\d\.]+\s*\1\s*;?\s*(\r?\n)/g;
-
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
